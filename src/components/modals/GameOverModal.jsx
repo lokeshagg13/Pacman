0b10a8c9e0a28f6a61f83b87fdd3e0b49a002cf6
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-22 22:26:11 +0530

    [UPDATE] - Show scores and high score on game over

--- src/components/modals/GameOverModal.jsx
+++ src/components/modals/GameOverModal.jsx
@@ -1,18 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+
 import StrongPacmanImage from "../../images/pacman/strong-pacman.png";
 import CompletionModal from "./commons/CompletionModal";
+import GameContext from "../../store/gameContext";
 
 function GameOverModal() {
+  const gameContext = useContext(GameContext);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    if (gameContext.gameStatus === "completed") {
+      const currentScore = gameContext.score;
+      const storedHighScore = localStorage.getItem("pacmanHighScore");
+      if (!storedHighScore || (currentScore > storedHighScore)) {
+        localStorage.setItem("pacmanHighScore", currentScore);
+        setHighScore(currentScore);
+        return;
+      }
+      setHighScore(storedHighScore);
+    }
+  }, [gameContext.gameStatus, gameContext.score]);
   return (
     <CompletionModal>
       <div className="game-over-modal">
-        <img
-          src={StrongPacmanImage}
-          alt="Game Over"
-          className="game-over-image"
-        />
-        <div>
+        <div className="game-over-header">
+          <img
+            src={StrongPacmanImage}
+            alt="Game Over"
+            className="game-over-image"
+          />
           <h2 className="game-over-title">Game Over</h2>
-          <h4 className="game-over-subt">Lets try again !!</h4>
+        </div>
+        <div className="score-section">
+          <div className="game-over-score">
+            <div className="score-title">Your Score: </div>
+            <div className="score-value">{gameContext.score}</div>
+          </div>
+          <div className="high-score">
+            <div className="high-score-title">High Score: </div>
+            <div className="high-score-value">{highScore}</div>
+          </div>
         </div>
       </div>
     </CompletionModal>
--- src/components/modals/SwipeAnimation.jsx
+++ src/components/modals/SwipeAnimation.jsx
@@ -4,7 +4,7 @@ function SwipeAnimation() {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-trans-black z-10">
       <h2 className="text-2xl font-bold mt-4 text-wheat">Swipe to move the player</h2>
-      <img src={SwipeAnimatioImage} className="max-w-200px"/>
+      <img src={SwipeAnimatioImage} alt="Swipe" className="max-w-200px"/>
     </div>
   );
 }
